ba62a54af29438ebfabf56925061af1d71307f4c
Garth N. Wells <gnw20@cam.ac.uk>
2025-06-30 10:05:00 +0100

    Simplify

--- src/vector.hpp
+++ src/vector.hpp
@@ -72,19 +72,28 @@ public:
   /// Create a distributed vector
   Vector(std::shared_ptr<const common::IndexMap> map, int bs)
       : _map(map), _bs(bs),
-        _scatterer(std::make_shared<common::Scatterer<>>(*_map, bs))
+        _scatterer(std::make_shared<common::Scatterer<>>(*_map, bs)),
+        _buffer_local(
+            thrust::device_vector<T>(_scatterer->local_buffer_size(), 0)),
+        _buffer_remote(
+            thrust::device_vector<T>(_scatterer->remote_buffer_size(), 0)),
+        _local_indices(
+            thrust::device_vector<std::int32_t>(_scatterer->local_indices())),
+        _remote_indices(
+            thrust::device_vector<std::int32_t>(_scatterer->remote_indices())),
+        _x(bs * (map->size_local() + map->num_ghosts()), 0)
   {
-    int size = bs * (map->size_local() + map->num_ghosts());
-    _x = thrust::device_vector<T>(size, 0);
+    // int size = bs * (map->size_local() + map->num_ghosts());
+    // _x = thrust::device_vector<T>(size, 0);
 
-    _buffer_local
-        = thrust::device_vector<T>(_scatterer->local_buffer_size(), 0);
-    _buffer_remote
-        = thrust::device_vector<T>(_scatterer->remote_buffer_size(), 0);
-    _local_indices
-        = thrust::device_vector<std::int32_t>(_scatterer->local_indices());
-    _remote_indices
-        = thrust::device_vector<std::int32_t>(_scatterer->remote_indices());
+    // _buffer_local
+    //     = thrust::device_vector<T>(_scatterer->local_buffer_size(), 0);
+    // _buffer_remote
+    //     = thrust::device_vector<T>(_scatterer->remote_buffer_size(), 0);
+    // _local_indices
+    //     = thrust::device_vector<std::int32_t>(_scatterer->local_indices());
+    // _remote_indices
+    //     = thrust::device_vector<std::int32_t>(_scatterer->remote_indices());
 
     auto scatterer_type = common::Scatterer<>::type::p2p;
     _request = _scatterer->create_request_vector(scatterer_type);
