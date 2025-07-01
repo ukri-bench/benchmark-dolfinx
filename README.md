bb27fb97d0db55420e60cc30b89a973fe8b7068e
Garth N. Wells <gnw20@cam.ac.uk>
2025-07-01 08:22:59 +0100

    CI runtime updates (#26)
    * ROCm 6.4.1
    
    * Test runtime update
    
    * Float type fixes
    
    * Database update

--- README.md
+++ README.md
@@ -118,17 +118,17 @@ Suggested options for running the test are listed below.
 
 Single-GPU basic test for correctness (small problem)
 ```bash
-bench_dolfinx --degree=5 --perturb_geom_fact=0.1 --mat_comp --ndofs=5000
+bench_dolfinx --float=64 --degree=5 --perturb_geom_fact=0.1 --mat_comp --ndofs=5000
 ```
 
 Single-GPU performance test (10M dofs)
 ```bash
-bench_dolfinx --degree=6 --ndofs=10000000 --qmode=1 --use_gauss
+bench_dolfinx --float=64 --degree=6 --ndofs=10000000 --qmode=1 --use_gauss
 ```
 
 Multi-GPU performance test (40M dofs)
 ```bash
-mpirun -n 4 bench_dolfinx --degree=6 --ndofs=10000000 --qmode=1 --use_gauss
+mpirun -n 4 bench_dolfinx --float=64  --degree=6 --ndofs=10000000 --qmode=1 --use_gauss
 ```
 
 ## License
--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <basix/finite-element.h>
 #include <boost/program_options.hpp>
 #include <dolfinx/mesh/generation.h>
+#include <format>
 #include <iostream>
 #include <json/json.h>
 #include <memory>
@@ -18,7 +19,6 @@
 
 using namespace dolfinx;
 namespace po = boost::program_options;
-using T = double;
 
 namespace
 {
@@ -69,12 +69,12 @@ std::array<std::int64_t, 3> compute_num_cells(std::int64_t ndofs, int degree,
 /// @param n
 /// @param geom_perturb_fact
 /// @return
-template <typename X>
-mesh::Mesh<X> create_mesh(MPI_Comm comm, std::array<std::int64_t, 3> n,
-                          X geom_perturb_fact)
+template <typename T>
+mesh::Mesh<T> create_mesh(MPI_Comm comm, std::array<std::int64_t, 3> n,
+                          T geom_perturb_fact)
 {
   mesh::Mesh<T> mesh0
-      = mesh::create_box<X>(comm, {{{0, 0, 0}, {1, 1, 1}}}, {n[0], n[1], n[2]},
+      = mesh::create_box<T>(comm, {{{0, 0, 0}, {1, 1, 1}}}, {n[0], n[1], n[2]},
                             mesh::CellType::hexahedron);
 
   if (geom_perturb_fact != 0.0)
@@ -98,30 +98,108 @@ mesh::Mesh<X> create_mesh(MPI_Comm comm, std::array<std::int64_t, 3> n,
   return benchdolfinx::ghost_layer_mesh(mesh0, celement);
 }
 
+template <typename T>
+void run_gpu(MPI_Comm comm, std::array<std::int64_t, 3> nx,
+             double geom_perturb_fact, int degree, int qmode, int nreps,
+             bool use_gauss, Json::Value& in_root)
+{
+  int rank(0), size(0);
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &size);
+
+  // Create mesh
+  spdlog::info("Mesh shape: {}x{}x{}", nx[0], nx[1], nx[2]);
+  auto mesh = std::make_shared<mesh::Mesh<T>>(
+      create_mesh<T>(comm, nx, geom_perturb_fact));
+
+  // Finite element for higher-order discretisation
+  auto element = basix::create_tp_element<T>(
+      basix::element::family::P, basix::cell::type::hexahedron, degree,
+      basix::element::lagrange_variant::gll_warped,
+      basix::element::dpc_variant::unset, false);
+
+  auto V = std::make_shared<fem::FunctionSpace<T>>(fem::create_functionspace(
+      mesh, std::make_shared<const fem::FiniteElement<T>>(element)));
+
+  auto topology = V->mesh()->topology_mutable();
+  int tdim = topology->dim();
+  // std::size_t ncells = mesh->topology()->index_map(tdim)->size_global();
+  std::size_t ndofs_global = V->dofmap()->index_map->size_global();
+
+  // Prepare and set Constants for the bilinear form
+  spdlog::debug("Define forms");
+  auto kappa = std::make_shared<fem::Constant<T>>(2.0);
+  auto f = std::make_shared<fem::Function<T>>(V);
+
+  // TODO: Handle float type in generated code
+  fem::Form<double> a = benchdolfinx::create_laplacian_form2(
+      V, {{"c0", kappa}}, qmode, use_gauss, degree);
+  fem::Form<double> L = benchdolfinx::create_laplacian_form1(
+      V, {{"w0", f}}, qmode, use_gauss, degree);
+
+  spdlog::debug("Interpolate (rank {})", rank);
+  f->interpolate(
+      [](auto x) -> std::pair<std::vector<T>, std::vector<std::size_t>>
+      {
+        std::vector<T> out;
+        for (std::size_t p = 0; p < x.extent(1); ++p)
+        {
+          auto dx = (x(0, p) - 0.5) * (x(0, p) - 0.5);
+          auto dy = (x(1, p) - 0.5) * (x(1, p) - 0.5);
+          out.push_back(1000 * std::exp(-(dx + dy) / 0.02));
+        }
+        return {out, {out.size()}};
+      });
+
+  int fdim = tdim - 1;
+  spdlog::debug("Create f->c on {}", rank);
+  topology->create_connectivity(fdim, tdim);
+  spdlog::debug("Done f->c on {}", rank);
+
+  auto dofmap = V->dofmap();
+  auto facets = mesh::exterior_facet_indices(*topology);
+  auto bdofs = fem::locate_dofs_topological(*topology, *dofmap, fdim, facets);
+  fem::DirichletBC<T> bc(1.3, bdofs, V);
+
+  benchdolfinx::laplace_action<T>(a, L, bc, degree, qmode, kappa->value[0],
+                                  nreps, use_gauss);
+}
 } // namespace
 
 int main(int argc, char* argv[])
 {
   // Program options
   po::options_description desc("Options");
-  desc.add_options()("help,h", "Print usage message")(
-      "benchmark,b", po::value<std::size_t>()->default_value(0),
-      "Test to run: 0=off, 1=correctness, 2=performance")(
-      "ndofs", po::value<std::size_t>()->default_value(1000),
-      "Number of degrees-of-freedom per MPI process")(
-      "qmode", po::value<std::size_t>()->default_value(1),
-      "Quadrature mode (0 or 1): qmode=0 has P+1 points in each direction,"
-      "qmode=1 has P+2 points in each direction.")(
-      "nreps", po::value<std::size_t>()->default_value(1000),
-      "Number of repetitions")("degree",
-                               po::value<std::size_t>()->default_value(3),
-                               "Polynomial degree \"P\" (2-7)")(
-      "mat_comp", po::bool_switch()->default_value(false),
-      "Compare result to matrix operator (slow with large ndofs)")(
-      "geom_perturb_fact", po::value<T>()->default_value(0.125),
-      "Randomly perturb the geometry (useful to check "
-      "correctness)")("use_gauss", po::bool_switch()->default_value(false),
-                      "Use Gauss quadrature rather than GLL quadrature");
+  desc.add_options()("help,h", "Print usage message")
+      //
+      ("benchmark,b", po::value<std::size_t>()->default_value(0),
+       "Test to run: 0=off, 1=correctness, 2=performance")
+      //
+      ("float", po::value<std::size_t>()->default_value(64),
+       "Float size (bits). 32 or 64.")
+      //
+      ("ndofs", po::value<std::size_t>()->default_value(1000),
+       "Number of degrees-of-freedom per MPI process")
+      //
+      ("qmode", po::value<std::size_t>()->default_value(1),
+       "Quadrature mode (0 or 1): qmode=0 has P+1 points in each direction,"
+       "qmode=1 has P+2 points in each direction.")
+      //
+      ("nreps", po::value<std::size_t>()->default_value(1000),
+       "Number of repetitions")
+      //
+      ("degree", po::value<std::size_t>()->default_value(3),
+       "Polynomial degree \"P\" (2-7)")
+      //
+      ("mat_comp", po::bool_switch()->default_value(false),
+       "Compare result to matrix operator (slow with large ndofs)")
+      //
+      ("geom_perturb_fact", po::value<double>()->default_value(0.125),
+       "Randomly perturb the geometry (useful to check "
+       "correctness)")
+      //
+      ("use_gauss", po::bool_switch()->default_value(false),
+       "Use Gauss quadrature rather than GLL quadrature");
 
   po::variables_map vm;
   po::store(po::command_line_parser(argc, argv)
@@ -142,11 +220,14 @@ int main(int argc, char* argv[])
     return 0;
   }
 
+  std::size_t float_size = vm["float"].as<std::size_t>();
+  if (float_size != 32 and float_size != 64)
+    throw std::runtime_error("Invalid float size. Must be 32 or 64.");
   std::size_t ndofs = vm["ndofs"].as<std::size_t>();
   std::size_t nreps = vm["nreps"].as<std::size_t>();
   std::size_t degree = vm["degree"].as<std::size_t>();
   bool matrix_comparison = vm["mat_comp"].as<bool>();
-  T geom_perturb_fact = vm["geom_perturb_fact"].as<T>();
+  double geom_perturb_fact = vm["geom_perturb_fact"].as<double>();
   bool use_gauss = vm["use_gauss"].as<bool>();
 
   // Quadrature mode (qmode=0: nq = P + 1, qmode=1: nq = P + 2)
@@ -174,32 +255,6 @@ int main(int argc, char* argv[])
     MPI_Comm_rank(comm, &rank);
     MPI_Comm_size(comm, &size);
 
-    // Create mesh
-    std::array<std::int64_t, 3> nx = compute_num_cells(ndofs, degree, size);
-    spdlog::info("Mesh shape: {}x{}x{}", nx[0], nx[1], nx[2]);
-    auto mesh = std::make_shared<mesh::Mesh<T>>(
-        create_mesh<T>(comm, nx, geom_perturb_fact));
-
-    // Finite element for higher-order discretisation
-    auto element = basix::create_tp_element<T>(
-        basix::element::family::P, basix::cell::type::hexahedron, degree,
-        basix::element::lagrange_variant::gll_warped,
-        basix::element::dpc_variant::unset, false);
-
-    auto V = std::make_shared<fem::FunctionSpace<T>>(fem::create_functionspace(
-        mesh, std::make_shared<const fem::FiniteElement<T>>(element)));
-
-    auto topology = V->mesh()->topology_mutable();
-    int tdim = topology->dim();
-    std::size_t ncells = mesh->topology()->index_map(tdim)->size_global();
-    std::size_t ndofs_global = V->dofmap()->index_map->size_global();
-
-    std::string fp_type = "float";
-    if (std::is_same_v<T, float>)
-      fp_type += "32";
-    else if (std::is_same_v<T, double>)
-      fp_type += "64";
-
     if (rank == 0)
     {
 #if defined(USE_CUDA) || defined(USE_HIP)
@@ -214,12 +269,13 @@ int main(int argc, char* argv[])
       std::cout << "Slice size: " << SLICE_SIZE << std::endl;
 #endif
       std::cout << "Number of ranks : " << size << std::endl;
-      std::cout << "Number of cells-global : " << ncells << std::endl;
-      std::cout << "Number of dofs-global : " << ndofs_global << std::endl;
-      std::cout << "Number of cells-rank : " << ncells / size << std::endl;
-      std::cout << "Number of dofs-rank : " << ndofs_global / size << std::endl;
+      // std::cout << "Number of cells-global : " << ncells << std::endl;
+      // std::cout << "Number of dofs-global : " << ndofs_global << std::endl;
+      // std::cout << "Number of cells-rank : " << ncells / size << std::endl;
+      // std::cout << "Number of dofs-rank : " << ndofs_global / size <<
+      // std::endl;
       std::cout << "Number of repetitions : " << nreps << std::endl;
-      std::cout << "Scalar Type: " << fp_type << std::endl;
+      // std::cout << "Scalar Type: " << fp_type << std::endl;
       std::cout << "XXXUse GLL: " << use_gauss << std::endl;
       std::cout << "Foo: " << matrix_comparison << std::endl;
       std::cout << "-----------------------------------" << std::endl;
@@ -232,48 +288,33 @@ int main(int argc, char* argv[])
     Json::Value& in_root = root["input"];
     in_root["p"] = (Json::UInt64)degree;
     in_root["mpi_size"] = size;
-    in_root["ncells"] = (Json::UInt64)ncells;
-    in_root["ndofs"] = (Json::UInt64)ndofs_global;
+    // in_root["ncells"] = (Json::UInt64)ncells;
+    // in_root["ndofs"] = (Json::UInt64)ndofs_global;
     in_root["nreps"] = (Json::UInt64)nreps;
-    in_root["scalar_type"] = fp_type;
+    in_root["scalar_size"] = (Json::UInt64)float_size;
     in_root["mat_comp"] = matrix_comparison;
 
-    // Prepare and set Constants for the bilinear form
-    spdlog::debug("Define forms");
-    auto kappa = std::make_shared<fem::Constant<T>>(2.0);
-    auto f = std::make_shared<fem::Function<T>>(V);
-    fem::Form<double> a = benchdolfinx::create_laplacian_form2(
-        V, {{"c0", kappa}}, qmode, use_gauss, degree);
-    fem::Form<double> L = benchdolfinx::create_laplacian_form1(
-        V, {{"w0", f}}, qmode, use_gauss, degree);
-
-    spdlog::debug("Interpolate (rank {})", rank);
-    f->interpolate(
-        [](auto x) -> std::pair<std::vector<T>, std::vector<std::size_t>>
-        {
-          std::vector<T> out;
-          for (std::size_t p = 0; p < x.extent(1); ++p)
-          {
-            auto dx = (x(0, p) - 0.5) * (x(0, p) - 0.5);
-            auto dy = (x(1, p) - 0.5) * (x(1, p) - 0.5);
-            out.push_back(1000 * std::exp(-(dx + dy) / 0.02));
-          }
-          return {out, {out.size()}};
-        });
-
-    int fdim = tdim - 1;
-    spdlog::debug("Create f->c on {}", rank);
-    topology->create_connectivity(fdim, tdim);
-    spdlog::debug("Done f->c on {}", rank);
-
-    auto dofmap = V->dofmap();
-    auto facets = mesh::exterior_facet_indices(*topology);
-    auto bdofs = fem::locate_dofs_topological(*topology, *dofmap, fdim, facets);
-    fem::DirichletBC<T> bc(1.3, bdofs, V);
+    std::array<std::int64_t, 3> nx = compute_num_cells(ndofs, degree, size);
 
 #if defined(USE_CUDA) || defined(USE_HIP)
-    benchdolfinx::laplace_action<T>(a, L, bc, degree, qmode, kappa->value[0],
-                                    nreps, use_gauss);
+    if (float_size == 32)
+    {
+      throw std::runtime_error("Float32 not implemented yet.");
+      // run_gpu<float>(comm, nx, geom_perturb_fact, degree, qmode, nreps,
+      //                use_gauss, in_root);
+    }
+    else if (float_size == 64)
+    {
+      run_gpu<double>(comm, nx, geom_perturb_fact, degree, qmode, nreps,
+                      use_gauss, in_root);
+    }
+    else
+    {
+      throw std::runtime_error(
+          std::format("Invalid float size {}. Must be 32 or 64.", float_size));
+    }
+#else
+    std::cout << "CPU version not implemented yet." << std::endl;
 #endif
     // if (rank == 0)
     // {
